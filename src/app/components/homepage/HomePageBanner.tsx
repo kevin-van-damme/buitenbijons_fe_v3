51647f1631aa6ca850c6124d317f6620db38cbfc
Kevin Van Damme <kevin.van.damme.pxl@protonmail.com>
2025-06-17 21:51:06 +0200

    focussing mobile first

--- src/app/components/homepage/HomePageBanner.tsx
+++ src/app/components/homepage/HomePageBanner.tsx
@@ -1,4 +1,12 @@
 const HomePageBanner = () => {
-  return <div>HomePageBanner</div>;
+  return (
+    <div className="mb-7 bg-slate-700 text-white px-2 py-6 lg:py-3">
+      <ul className="flex justify-around text-md max-w-7xl mx-auto">
+        <li>Make Memories</li>
+        <li>Reconnect to Nature</li>
+        <li>Explore New Horizons</li>
+      </ul>
+    </div>
+  );
 };
 export default HomePageBanner;
--- src/app/components/homepage/HomePagePopularSelection.tsx
+++ src/app/components/homepage/HomePagePopularSelection.tsx
@@ -1,4 +1,13 @@
 const HomePagePopularSelection = () => {
-  return <div>HomePagePopularSelection</div>;
+  return (
+    <div className="px-2">
+      <h3 className="text-4xl font-bold">Popular Locations</h3>
+      <ul className="flex flex-row gap-4">
+        <li>Brugge</li>
+        <li>Durbuy</li>
+        <li>Namur</li>
+      </ul>
+    </div>
+  );
 };
 export default HomePagePopularSelection;
