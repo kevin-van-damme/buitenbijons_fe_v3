4c8b91f3a15e1ccb07851a77db1ca21f480b8b06
Kevin Van Damme <kevin.van.damme.pxl@protonmail.com>
2025-06-18 11:59:47 +0200

    adjusting css to make it responsive

--- src/app/components/homepage/HomePagePopularSelection.tsx
+++ src/app/components/homepage/HomePagePopularSelection.tsx
@@ -1,8 +1,8 @@
 const HomePagePopularSelection = () => {
   return (
-    <div className="">
-      <h3 className="text-4xl font-bold">Popular Locations</h3>
-      <ul className="flex flex-row gap-4">
+    <div className="lg:w-6xl lg:mx-auto md:w-7xl md:mx-2">
+      <h3 className="text-4xl font-bold text-center mb-5 lg:text-left lg:mb-5 md:mb-5 md:text-left">Popular Locations</h3>
+      <ul className="flex flex-col gap-4 items-center lg:flex-row md:flex-row">
         <li>Brugge</li>
         <li>Durbuy</li>
         <li>Namur</li>
