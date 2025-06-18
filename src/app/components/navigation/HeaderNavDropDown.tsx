0ad99945ebb4bcd0ef6c255167a03b0ed3e7e846
Kevin Van Damme <kevin.van.damme.pxl@protonmail.com>
2025-06-18 11:51:25 +0200

    added animation to dropdown and changed the lg and md formats to hidden so that it won't show on other dimensions

--- src/app/components/navigation/HeaderNavDropDown.tsx
+++ src/app/components/navigation/HeaderNavDropDown.tsx
@@ -2,13 +2,13 @@
 import { useState } from "react";
 import Link from "next/link";
 import { Menu, X } from "lucide-react";
+import { motion, AnimatePresence } from "framer-motion";
 
 const HeaderNavDropDown = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <>
-      {/* DEKSTOP VERSIE */}
-      <nav className="hidden lg:flex items-center">
+      <nav className="hidden lg:flex md:flex md:mx-2 md:text-2xl items-center">
         <ul className="flex gap-3">
           <li>
             <Link href="/aboutpage" className="hover:text-slate-700 transition-all">
@@ -32,36 +32,45 @@ const HeaderNavDropDown = () => {
           </li>
         </ul>
       </nav>
-      {/* GSM VERSIE */}
-      <div className="lg:hidden flex items-center">
-        <button onClick={() => setIsOpen(!isOpen)}>{isOpen ? <X size={50} /> : <Menu size={50} />}</button>
+      <div className="lg:hidden md:hidden flex items-center">
+        <button onClick={() => setIsOpen(!isOpen)} className="hover:cursor-pointer">
+          {isOpen ? <X size={47} /> : <Menu size={47} />}
+        </button>
       </div>
-      {isOpen && (
-        <div className="absolute top-24 right-5 bg-white shadow-lg rounded-lg p-5 w-60 z-50">
-          <ul className="flex flex-col gap-4">
-            <li>
-              <Link href="/aboutpage" onClick={() => setIsOpen(false)} className="hover:text-slate-700 transition-all">
-                About
-              </Link>
-            </li>
-            <li>
-              <Link href="/all-campings" onClick={() => setIsOpen(false)} className="hover:text-slate-700 transition-all">
-                Campings
-              </Link>
-            </li>
-            <li>
-              <Link href="/login" onClick={() => setIsOpen(false)} className="hover:text-slate-700 transition-all">
-                Login
-              </Link>
-            </li>
-            <li>
-              <Link href="/register" onClick={() => setIsOpen(false)} className="hover:text-slate-700 transition-all">
-                Sign Up
-              </Link>
-            </li>
-          </ul>
-        </div>
-      )}
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -20 }}
+            transition={{ duration: 0.3 }}
+            className="absolute top-24 right-5 bg-white shadow-lg rounded-lg p-5 w-60 z-50 lg:hidden md:hidden"
+          >
+            <ul className="flex flex-col gap-4 ">
+              <li>
+                <Link href="/aboutpage" onClick={() => setIsOpen(false)} className="hover:text-slate-700 transition-all">
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link href="/all-campings" onClick={() => setIsOpen(false)} className="hover:text-slate-700 transition-all">
+                  Campings
+                </Link>
+              </li>
+              <li>
+                <Link href="/login" onClick={() => setIsOpen(false)} className="hover:text-slate-700 transition-all">
+                  Login
+                </Link>
+              </li>
+              <li>
+                <Link href="/register" onClick={() => setIsOpen(false)} className="hover:text-slate-700 transition-all">
+                  Sign Up
+                </Link>
+              </li>
+            </ul>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </>
   );
 };
